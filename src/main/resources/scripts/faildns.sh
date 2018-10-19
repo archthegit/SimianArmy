4d682cf6853f4d81cef70fba1b8e7c5900d34d01
Archana Pradeep <archieprad@gmail.com>
2018-10-19 10:00:57 +0800

    Fixing bug with fail dns

--- src/main/resources/scripts/faildns.sh
+++ src/main/resources/scripts/faildns.sh
@@ -2,5 +2,5 @@
 # Script for FailDns Chaos Monkey
 
 # Block all traffic on port 53
-iptables -A INPUT -p tcp -m tcp --dport 53 -j DROP
-iptables -A INPUT -p udp -m udp --dport 53 -j DROP
+iptables -A OUTPUT -p tcp -m tcp --dport 53 -j DROP
+iptables -A OUTPUT -p udp -m udp --dport 53 -j DROP
